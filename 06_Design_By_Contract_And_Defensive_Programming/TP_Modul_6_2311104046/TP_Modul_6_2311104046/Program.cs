6cfb946d7449fc3411c05536ae504c1d4fde977a
zclimaxsp <dhiemasikhsan@gmail.com>
2025-03-21 09:13:56 +0700

    Menambahkan class SayaTubeVideo

--- 06_Design_By_Contract_And_Defensive_Programming/TP_Modul_6_2311104046/TP_Modul_6_2311104046/Program.cs
+++ 06_Design_By_Contract_And_Defensive_Programming/TP_Modul_6_2311104046/TP_Modul_6_2311104046/Program.cs
@@ -1,1 +1,44 @@
-﻿
+﻿using System;
+
+class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        if (string.IsNullOrEmpty(title) || title.Length > 100)
+            throw new ArgumentException("Judul video tidak boleh null dan maksimal 100 karakter.");
+
+        Random rand = new Random();
+        this.id = rand.Next(10000, 99999); // 5 digit random ID
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        if (count > 10000000 || count < 0)
+            throw new ArgumentException("Penambahan play count maksimal 10.000.000 dan tidak boleh negatif.");
+
+        try
+        {
+            checked
+            {
+                this.playCount += count;
+            }
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Error: Play count melebihi batas integer.");
+        }
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("Video ID: " + this.id);
+        Console.WriteLine("Title: " + this.title);
+        Console.WriteLine("Play Count: " + this.playCount);
+    }
+}
