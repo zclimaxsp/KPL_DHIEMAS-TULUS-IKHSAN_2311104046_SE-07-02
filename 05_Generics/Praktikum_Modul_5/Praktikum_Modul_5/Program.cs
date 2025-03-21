dfbff35fff95da13f9c6aaff189e4579120b5735
zclimaxsp <dhiemasikhsan@gmail.com>
2025-03-21 08:19:44 +0700

    Menambahkan file baru dan update perubahan lainnya

--- 05_Generics/Praktikum_Modul_5/Praktikum_Modul_5/Program.cs
+++ 05_Generics/Praktikum_Modul_5/Praktikum_Modul_5/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+public class Program
+{
+    public static void Main()
+    {
+        int[] arr = { 0, 1, 2, 3, 4 };
+        List<int> list = new List<int>();
+        for (int x = 5; x < 10; x++)
+        {
+            list.Add(x);
+        }
+        ProcessItems<int>(arr);
+        ProcessItems<int>(list);
+    }
+    static void ProcessItems<T>(IList<T> coll)
+    {
+        // IsReadOnly returns True for the array and False for the List.
+        System.Console.WriteLine
+        ("IsReadOnly returns {0} for this collection.",
+        coll.IsReadOnly);
+        // The following statement causes a run-time exception for the
+        // array, but not for the List.
+        //coll.RemoveAt(4);
+        foreach (T item in coll)
+        {
+            System.Console.Write(item.ToString() + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
