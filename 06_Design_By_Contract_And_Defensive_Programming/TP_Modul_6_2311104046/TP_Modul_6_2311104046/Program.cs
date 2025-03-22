ad927127a9b9d0c95bbc5183fb6086cd631a5e46
zclimaxsp <151652931+zclimaxsp@users.noreply.github.com>
2025-03-22 17:01:19 +0700

    mengubah nama di sintaks

--- 06_Design_By_Contract_And_Defensive_Programming/TP_Modul_6_2311104046/TP_Modul_6_2311104046/Program.cs
+++ 06_Design_By_Contract_And_Defensive_Programming/TP_Modul_6_2311104046/TP_Modul_6_2311104046/Program.cs
@@ -48,7 +48,7 @@ class Program
 {
     static void Main()
     {
-        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – [NAMA_PRAKTIKAN]");
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – [dhiemas_2311104046]");
 
         video.PrintVideoDetails();
 
