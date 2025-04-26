f635d1af4f334e97e92fa6495923b11aec6b776e
zclimaxsp <dhiemasikhsan@gmail.com>
2025-04-26 22:01:30 +0700

    menambahkan API Movies

---
+++ 09_API_Design_dan_Construction_Using_Swagger/tjmodul9_2311104046/tjmodul9_2311104046/Controllers/MoviesController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using tjmodul9_2311104046.Models;
+
+namespace tjmodul9_2311104046.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private static List<Movie> movies = new List<Movie>
+        {
+            new Movie
+            {
+                Title = "The Shawshank Redemption",
+                Director = "Frank Darabont",
+                Stars = new List<string> { "Tim Robbins", "Morgan Freeman" },
+                Description = "Two imprisoned men bond over a number of years."
+            },
+            new Movie
+            {
+                Title = "The Godfather",
+                Director = "Francis Ford Coppola",
+                Stars = new List<string> { "Marlon Brando", "Al Pacino" },
+                Description = "The aging patriarch of an organized crime dynasty transfers control."
+            },
+            new Movie
+            {
+                Title = "The Dark Knight",
+                Director = "Christopher Nolan",
+                Stars = new List<string> { "Christian Bale", "Heath Ledger" },
+                Description = "When the menace known as the Joker wreaks havoc and chaos."
+            }
+        };
+
+        [HttpGet]
+        public ActionResult<List<Movie>> Get()
+        {
+            return movies;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Movie> Get(int id)
+        {
+            if (id < 0 || id >= movies.Count)
+            {
+                return NotFound();
+            }
+            return movies[id];
+        }
+
+        [HttpPost]
+        public ActionResult<List<Movie>> Post(Movie movie)
+        {
+            movies.Add(movie);
+            return movies;
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<List<Movie>> Delete(int id)
+        {
+            if (id < 0 || id >= movies.Count)
+            {
+                return NotFound();
+            }
+            movies.RemoveAt(id);
+            return movies;
+        }
+    }
+}
---
+++ 09_API_Design_dan_Construction_Using_Swagger/tjmodul9_2311104046/tjmodul9_2311104046/Models/Models.cs
@@ -0,0 +1,16 @@
+﻿namespace tjmodul9_2311104046.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public List<string> Stars { get; set; }
+        public string Description { get; set; }
+
+        public Movie()
+        {
+            Stars = new List<string>();
+        }
+    }
+}
+
