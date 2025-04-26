5ab6ceefe60da35f4a988856d8cc472da73b4590
zclimaxsp <dhiemasikhsan@gmail.com>
2025-04-26 21:34:31 +0700

    commit pertama

---
+++ 09_API_Design_dan_Construction_Using_Swagger/tjmodul9_2311104046/tjmodul9_2311104046/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace tjmodul9_2311104046
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
