9fc228f26d46ff98db798a1c739b14094b6b6057
zclimaxsp <dhiemasikhsan@gmail.com>
2025-04-30 10:24:48 +0700

    membuat classlibrary dan consoleapp aljabar

---
+++ 10_Library_Construction/tpmodul10_2311104046/AljabarLibraries/Aljabar.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AljabarLibraries
+{
+    public class Aljabar
+    {
+        public static double[] AkarPersamaanKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+            double c = persamaan[2];
+
+            double diskriminan = b * b - 4 * a * c;
+
+            if (diskriminan < 0)
+            {
+                throw new Exception("Akar tidak real");
+            }
+
+            double akar1 = (-b + Math.Sqrt(diskriminan)) / (2 * a);
+            double akar2 = (-b - Math.Sqrt(diskriminan)) / (2 * a);
+
+            return new double[] { akar1, akar2 };
+        }
+
+        public static double[] HasilKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+
+            double hasilA = a * a;        // x^2
+            double hasilB = 2 * a * b;    // x
+            double hasilC = b * b;        // konstanta
+
+            return new double[] { hasilA, hasilB, hasilC };
+        }
+    }
+}
---
+++ 10_Library_Construction/tpmodul10_2311104046/MainProgram/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AljabarLibraries;
+
+namespace MainProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Memanggil fungsi HasilKuadrat
+            double[] hasilKuadrat = Aljabar.HasilKuadrat(new double[] { 2, -3 });
+            Console.WriteLine("Hasil Kuadrat dari 2x - 3:");
+            Console.WriteLine($"{hasilKuadrat[0]}x^2 + {hasilKuadrat[1]}x + {hasilKuadrat[2]}");
+
+            // Memanggil fungsi AkarPersamaanKuadrat
+            double[] akar = Aljabar.AkarPersamaanKuadrat(new double[] { 1, -3, -10 });
+            Console.WriteLine("\nAkar-akar dari persamaan x^2 - 3x - 10:");
+            Console.WriteLine($"x1 = {akar[0]}, x2 = {akar[1]}");
+        }
+    }
+}
