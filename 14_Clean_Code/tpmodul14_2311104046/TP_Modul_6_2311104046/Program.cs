8bd33e1b027eb1e0b2edcf8f8eab8ba151b2a8c3
zclimaxsp <dhiemasikhsan@gmail.com>
2025-05-17 14:50:34 +0700

    menambahkan salinan modul 6 untuk clean code modul 16

---
+++ 14_Clean_Code/tpmodul14_2311104046/TP_Modul_6_2311104046/Program.cs
@@ -0,0 +1,23 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            SayaTubeVideo video1 = new SayaTubeVideo("Tutorial Design By Contract – Dhiemas Tulus Ikhsan");
+            video1.IncreasePlayCount(5000000);
+            video1.PrintVideoDetails();
+
+            SayaTubeVideo video2 = new SayaTubeVideo("Test Overflow");
+            for (int i = 0; i < 10; i++)
+            {
+                video2.IncreasePlayCount(int.MaxValue);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Exception: {ex.Message}");
+        }
+    }
+}
