0bc94ecfe05422fcbde5818672c204c55157b3fd
zclimaxsp <dhiemasikhsan@gmail.com>
2025-06-13 22:29:19 +0700

    laporan laporan mepettt

---
+++ 12_Performance_Analysis_Unit_Testing_dan_Debugging/modul12/modul12/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace modul12
+{
+    public class Prgram
+    {
+        public static void Main()
+        {
+
+        }
+        public static String DetermineGrade(int score)
+        {
+            Dictionary<int, String> GradeMap = new Dictionary<int, string>()
+            {
+                {90, "A"},
+                {80, "B"},
+                {70, "C"},
+                {60, "D"},
+                {0, "E"},
+            };
+            foreach (var kv in GradeMap.OrderByDescending(kv => kv.Key))
+            {
+                if (score >= kv.Key)
+                {
+                    return kv.Value;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ 12_Performance_Analysis_Unit_Testing_dan_Debugging/modul12/unit test/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace unit_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            String? result = modul12.Program
+        }
+    }
+}
