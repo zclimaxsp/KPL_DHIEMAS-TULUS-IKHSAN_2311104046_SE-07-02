633b05787372018d4df780aa94b62d7ba21f7175
zclimaxsp <dhiemasikhsan@gmail.com>
2025-06-13 16:53:13 +0700

    membuat jurnal modul 10

---
+++ 10_Library_Construction/tjmodul10_2311104076/MatematikaLibraries/Matematika.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Text;
+
+namespace MatematikaLibraries
+{
+    public class Matematika
+    {
+        public int FPB(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        public int KPK(int a, int b)
+        {
+            return (a * b) / FPB(a, b);
+        }
+
+        public string Turunan(int[] persamaan)
+        {
+            StringBuilder hasil = new StringBuilder();
+            int pangkat = persamaan.Length - 1;
+
+            for (int i = 0; i < persamaan.Length - 1; i++)
+            {
+                int koefisien = persamaan[i] * pangkat;
+                if (koefisien == 0)
+                {
+                    pangkat--;
+                    continue;
+                }
+
+                if (hasil.Length > 0 && koefisien > 0)
+                    hasil.Append(" + ");
+
+                if (koefisien < 0)
+                    hasil.Append(" - ");
+
+                int absKoef = Math.Abs(koefisien);
+
+                if (absKoef != 1 || pangkat - 1 == 0)
+                    hasil.Append(absKoef);
+
+                if (pangkat - 1 > 0)
+                    hasil.Append("x");
+
+                if (pangkat - 1 > 1)
+                    hasil.Append(pangkat - 1);
+
+                pangkat--;
+            }
+
+            return hasil.ToString();
+        }
+
+        public string Integral(int[] persamaan)
+        {
+            StringBuilder hasil = new StringBuilder();
+            int pangkat = persamaan.Length - 1;
+
+            for (int i = 0; i < persamaan.Length; i++)
+            {
+                int koefisien = persamaan[i];
+                int pangkatBaru = pangkat + 1;
+                double hasilKoef = (double)koefisien / pangkatBaru;
+
+                if (hasil.Length > 0 && hasilKoef > 0)
+                    hasil.Append(" + ");
+
+                if (hasilKoef < 0)
+                    hasil.Append(" - ");
+
+                double absKoef = Math.Abs(hasilKoef);
+
+                if (absKoef != 1 || pangkatBaru == 0)
+                    hasil.Append(absKoef);
+
+                if (pangkatBaru > 0)
+                    hasil.Append("x");
+
+                if (pangkatBaru > 1)
+                    hasil.Append(pangkatBaru);
+
+                pangkat--;
+            }
+
+            hasil.Append(" + C");
+
+            return hasil.ToString();
+        }
+    }
+}
---
+++ 10_Library_Construction/tjmodul10_2311104076/ShowCase/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using MatematikaLibraries;
+
+namespace ShowCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matematika math = new Matematika();
+
+            Console.WriteLine("=== PROGRAM PERHITUNGAN MATEMATIKA ===\n");
+
+            Console.WriteLine(">> FUNGSI FPB");
+            Console.WriteLine("FPB dari 60 dan 45 = " + math.FPB(60, 45));
+            Console.WriteLine(new string('-', 40));
+
+            Console.WriteLine(">> FUNGSI KPK");
+            Console.WriteLine("KPK dari 12 dan 8 = " + math.KPK(12, 8));
+            Console.WriteLine(new string('-', 40));
+
+            int[] persamaan1 = { 1, 4, -12, 9 };
+            Console.WriteLine(">> TURUNAN POLINOMIAL");
+            Console.WriteLine("Persamaan awal   : x^3 + 4x^2 - 12x + 9");
+            Console.WriteLine("Hasil Turunan    : " + math.Turunan(persamaan1));
+            Console.WriteLine(new string('-', 40));
+
+            int[] persamaan2 = { 4, 6, -12, 9 };
+            Console.WriteLine(">> INTEGRAL POLINOMIAL");
+            Console.WriteLine("Persamaan awal   : 4x^3 + 6x^2 - 12x + 9");
+            Console.WriteLine("Hasil Integral   : " + math.Integral(persamaan2));
+            Console.WriteLine(new string('-', 40));
+
+            Console.WriteLine("\nTekan Enter untuk keluar...");
+            Console.ReadLine();
+        }
+
+    }
+}
